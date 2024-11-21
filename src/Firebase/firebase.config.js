8a3232347c1f8aac283d5851290591baa4069a57
nahin440 <rafironin@gmail.com>
2024-11-21 07:36:33 +0600

    Environment Variables done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,14 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD67iR2XXqBA6sdzsiv-UZnoe6q9S3Xdas",
-  authDomain: "winter-clothing-donation-cbfda.firebaseapp.com",
-  projectId: "winter-clothing-donation-cbfda",
-  storageBucket: "winter-clothing-donation-cbfda.firebasestorage.app",
-  messagingSenderId: "584397398009",
-  appId: "1:584397398009:web:1fa0358110c41b678fa728"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+export default app
